050d177243c35930522ae4ecbbd95f0214861372
George Otubu <george.otubu@microfocus.com>
2021-07-23 13:30:07 -0500

    First Commit

---
+++ etc/jboss4-support/src/com/fortify/samples/riches/database/DatabaseController.java
@@ -0,0 +1,31 @@
+/***********************************************************************
+ * (C) Copyright 2005  Fortify Software Inc.   All Rights Reserved.
+ *
+ *  Copyright 2005 Fortify Software, Inc. All rights reserved.
+ *  This software is provided solely in connection with the terms
+ *  of the license agreement.  Any other use without the prior express
+ *  written permission of Fortify Software is completely prohibited.
+ ************************************************************************/
+
+package com.fortify.samples.riches.database;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+/* $File$ 
+ * Created by: adam on Aug 8, 2005
+ *
+ * Details of last change:
+ * $Author$
+ * $DateTime$
+ * $Revision$
+ * $Change$
+ */
+
+public class DatabaseController implements ServletContextListener {
+
+	// do nothing, the functionality normally provided in this class is handled by JBoss MBeans
+
+	public void contextInitialized(ServletContextEvent servletContextEvent) { }
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {	}
+}
+
